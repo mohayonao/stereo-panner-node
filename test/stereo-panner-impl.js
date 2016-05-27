4bfb9055fcc0258969ace2fd1ca7e4e73c4376e2
mohayonao <mohayonao@gmail.com>
2016-05-28 05:30:09 +0900

    update dev tools

--- test/stereo-panner-impl.js
+++ test/stereo-panner-impl.js
@@ -1,4 +1,4 @@
-var assert = require("power-assert");
+var assert = require("assert");
 var StereoPannerImpl = require("../lib/stereo-panner-impl");
 
 describe("StereoPannerImpl", function() {
--- test/stereo-panner-node.js
+++ test/stereo-panner-node.js
@@ -1,4 +1,4 @@
-var assert = require("power-assert");
+var assert = require("assert");
 var StereoPannerNode = require("../lib/stereo-panner-node");
 
 describe("StereoPannerNode", function() {
